300f0c4d9f0f8ec6436990397ce274ff3ab6dfa3
Inokcode <regilearn@gmail.com>
2021-07-26 22:33:00 +0530

    MVC added

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,20 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
+
 const router = express.Router();
 
 //blog routes
-router.get('/', (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/', blogController.blog_index);
 
 //submit
-router.post('/', (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect('/blogs');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post('/', blogController.blog_create_post);
 
 // New Blog post
-router.get('/create', (req, res) => {
-  res.render('create', { title: 'Create a new Blog' });
-});
+router.get('/create', blogController.blog_create_get);
 
 //single blog Item
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  // console.log(id);
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details' });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get('/:id', blogController.blog_details);
 
-router.delete(':id', (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: '/blogs' }); //when browser send a requset u cant do noraml re direct u have to send object
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
